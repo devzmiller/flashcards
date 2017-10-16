a8f828491093afef25bb463feee644c75da871a2
Devin Miller & Marlo Ito <pair+devzmiller.marloito@devbootcamp.com>
2017-10-16 12:20:19 -0700

    add guess results handling

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,7 +1,11 @@
 class GuessesController < ApplicationController
   def create
+    params.permit!
     @card = Card.find(params[:card_id])
     @guess = Guess.new(params[:guess])
-    @guess.correct
+    @guess.card = @card
+    @guess.check_guess
+    @guess.save
+    render 'cards/show'
   end
 end
